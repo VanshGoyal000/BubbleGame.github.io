e0208bc5e0d26a0c555d895513968a5d4472a01e
Vansh Goyal <135618994+VanshGoyal000@users.noreply.github.com>
2023-08-24 10:56:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-var timer = 61;
+var timer = 60;
 var score = 0;
 var hitrn = 0;
 var fscore;
 
 // jispe click karoge wo element par event raise hoga , aur event listner na milne par event element ke parent par listner dhundega wha bhi na milne par event parent ke parent ke parent par listner dhundega
 
-function increaseScore(){
+function increaseScore() {
     fscore = score += 10;
     document.querySelector("#scoreval").textContent = score;
-    
 }
 
-function makebubble(){
+function makebubble() {
+    var clutter = "";
 
-var clutter ="";
-
-for (var i = 0; i <= 101; i++) {
-    var rn = Math.floor(Math.random()*10)
-    // floor remove decimar and math.random generate any number btw 0 to 1 so we multiply it by 10
-    clutter += `<div class="bubble">${rn}</div>`;
-    // yha ise store karana jaruri hai isliye ise var bnake clutter name se save kar diya
-    // agar ham = lagate he to wha pichla div remove hota rhega and nya banta rhega to isliye hamne use kiya += ka jo pichla save kara rhega aur agla bnata rhega
-    
-}
-
-document.querySelector("#pbtm").innerHTML = clutter;
-}
-function runtimer(){
-    var timerit = setInterval(function(){
-        if(timer>0){
-        timer--;
-        document.querySelector("#timervalue").textContent=timer;
+    for (var i = 0; i <= 101; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        // floor remove decimar and math.random generate any number btw 0 to 1 so we multiply it by 10
+        clutter += `<div class="bubble">${rn}</div>`;
+        // yha ise store karana jaruri hai isliye ise var bnake clutter name se save kar diya
+        // agar ham = lagate he to wha pichla div remove hota rhega and nya banta rhega to isliye hamne use kiya += ka jo pichla save kara rhega aur agla bnata rhega
     }
-    else{
-        document.querySelector("#pbtm").innerHTML = `<h1> GAME OVER!</h1> <br> <h2>Score ${fscore} </h2> `;
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+function runtimer() {
+    var timerit = setInterval( function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timervalue").textContent = timer;
+        }
+        document.querySelector(
+            "#pbtm"
+        ).innerHTML = `<h1> GAME OVER!</h1> <br> <h2>Score ${fscore} </h2> `;
         clearInterval(timerint);
-    }
-    },1000);
+    
+    } , 1000);
 }
-function getnewhit(){
-hitrn = Math.floor(Math.random()*10)
-document.querySelector("#hitval").textContent=hitrn;
+function getnewhit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
 }
 
-document.querySelector("#pbtm").addEventListener("click" , function(details){
-var clickednum = Number( details.target.textContent);
-if(clickednum == hitrn){
-    var audio = new Audio("correct answer.mp3");
+document.querySelector("#pbtm").addEventListener("click", function (details) {
+    var clickednum = Number(details.target.textContent);
+    if (clickednum == hitrn) {
+        var audio = new Audio("correct answer.mp3");
         audio.play();
-    increaseScore();
-    makebubble();
-    getnewhit();
-}
-       else {
+        increaseScore();
+        makebubble();
+        getnewhit();
+    }
+    else {
         var audio = new Audio("wrong answer.mp3");
         audio.play();
     }
-}
+});
 
-})
-
-getnewhit()
+getnewhit();
 runtimer();
 makebubble();
